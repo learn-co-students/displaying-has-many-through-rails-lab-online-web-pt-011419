55c7c22f7af08a0b50d8baa50b854eb6c53fcebf
NYHolzer <nissan.holzer@gmail.com>
2019-07-04 12:53:00 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,22 @@
+class Appointment < ApplicationRecord
+    belongs_to :patient
+    belongs_to :doctor
+
+    def doctor_name
+        self.doctor.name
+    end
+
+    def patient_name
+        self.patient.name
+    end
+
+    # => "December 04, 2007 00:00"
+    def convert_datetime_to_long_format
+        self.appointment_datetime.to_formatted_s(:long)
+    end
+
+    def convert_datetime_to_readable
+        self.convert_datetime_to_long_format.split.insert(3,"at").join(" ")
+    end
+
+end
